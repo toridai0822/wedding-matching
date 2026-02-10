0b86477f751bce1137df3a4f9b6127f0c3edc145
toridai0822 <ketiketidenki@gmail.com>
2026-02-10 14:46:41 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
-// ⚠️ ここにあなたのSupabaseの情報を入力してください ⚠️
+const SUPABASE_URL = "https://ylzomspzmjkabxhxupuf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_sz2A_mEZOn4LvE_I3ul53g_YzaqL5m2";
 
-// ステップ4-5でコピーした「Project URL」をここに貼り付け
-const SUPABASE_URL = 'https://xyfzompuzpkbbdznhuqvf.supabase.co';
-
-// ステップ4-5でコピーした「anon public key」をここに貼り付け
-const SUPABASE_ANON_KEY = 'sb_ccb1t4ab1a_s2bk_stzcntkct6_1zh13y_YeaaLKSc5BH3ZsQwgyZn0GXXTmDdmZoXiOO7tBHN8vOMGxpwOuLJh9zhNKgB4JHI9NpL5WqZCd0vkQ';
-
-// Supabaseクライアントの初期化
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
