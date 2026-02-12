c3b02663b1b7f02619d90429d7f503e4cf6945e6
toridai0822 <ketiketidenki@gmail.com>
2026-02-12 09:41:59 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://ylzomspzmjkabxhxupuf.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_sz2A_mEZOn4LvE_I3ul53g_YzaqL5m2";
 
-const supabaseClient = window.supabase.createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
