6dcf4c9e1af03a12086db960cb088559ccc2032f
toridai0822 <ketiketidenki@gmail.com>
2026-02-12 09:54:30 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
-const SUPABASE_URL = "https://ylzomspzmjkabxhxupuf.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_sz2A_mEZOn4LvE_I3ul53g_YzaqL5m2";
+// Supabase 設定
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+window.SUPABASE_URL = "https://ylzomspzmjkabxhxupuf.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_sz2A_mEZOn4LvE_I3ul53g_YzaqL5m2";
+
+// グローバルに1回だけ作る
+window.supabaseClient = window.supabase.createClient(
+  window.SUPABASE_URL,
+  window.SUPABASE_ANON_KEY
 );
